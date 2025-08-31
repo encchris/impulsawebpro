8d6ebaf947612140f3ef48f62f80f5c84996c8cb
AngelLopez03 <angellopezalarcon832@gmail.com>
2025-08-31 13:46:23 -0300

    Agregamos la lógica para renderizar el código

--- src/workArea/workArea.js
+++ src/workArea/workArea.js
@@ -1,5 +1,33 @@
 //main-root
+import { MapApi } from "../api/nodeMap.js";
+const Node = new MapApi;
 
 export function renderWorkArea() {
-    console.log("WorkArea");
+    const main = document.querySelector('#main-root');
+    main.innerHTML = "";
+    
+    Node.getParents.forEach(value => {
+        main.appendChild(createHTML(value));
+    })
+
+    function createHTML(value) {
+        //Generación del código HTML
+        const element = document.createElement(value.item);
+        element.dataset.item = value.identifier;
+
+        if (value.children && value.children.length > 0) { // Si tiene hijos
+            // Agrega todos los hijos
+            value.children.forEach(childId => {
+                const childNode = Node.nodeById(childId);
+                const childElement = createHTML(childNode);
+                if (childElement) {
+                    element.appendChild(childElement);
+                }
+            });
+            
+        } else {
+            element.textContent = "Modificar valor";
+        }
+        return element;
+    }
 }
